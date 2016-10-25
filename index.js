4792a7e174f8d01fd94cdd05e4b688973e5879b0
watanabeyu <fight.ippatu5648@gmail.com>
2016-10-25 17:23:46 +0900

    rewrite

--- index.js
+++ index.js
@@ -12,59 +12,41 @@ exports.handler = function(event,context,callback){
   /* youtube */
   if(url.match(/youtube|youtu\.be/i)){
     oEmbed = `https://www.youtube.com/oembed?url=${url}&format=json`;
-    html = "html";
-    thumbnail = "thumbnail_url";
   }
 
   /* soundcloud */
   if(url.match(/soundcloud/i)){
     oEmbed = `https://soundcloud.com/oembed?url=${url}&format=json`;
-    html = "html";
-    thumbnail = "thumbnail_url";
   }
 
   /* mixcloud */
   if(url.match(/mixcloud/i)){
     oEmbed = `https://www.mixcloud.com/oembed/?url=${url}&format=json`;
-    html = "embed";
-    thumbnail = "image";
   }
 
   /* vimeo */
   if(url.match(/vimeo/i)){
     oEmbed = `https://vimeo.com/api/oembed.json?url=${url}&format=json`;
-    html = "html";
-    thumbnail = "thumbnail_url";
   }
 
   /* slideshare */
   if(url.match(/slideshare/i)){
     oEmbed = `https://www.slideshare.net/api/oembed/2?url=${url}&format=json`;
-    html = "html";
-    thumbnail = "thumbnail";
   }
 
   /* twitter */
   if(url.match(/twitter/i)){
     oEmbed = `https://publish.twitter.com/oembed?url=${url}&format=json`;
-    html = "html";
-    thumbnail = "thumbnail_url";
   }
 
   /* instagram */
   if(url.match(/instagram|instagr\.am/i)){
     url = url.replace(/^(.+)\?.+$/,"$1");
     oEmbed = `https://api.instagram.com/oembed?url=${url}&format=json`;
-    html = "html";
-    thumbnail = "thumbnail_url";
   }
 
   request(oEmbed,function(error,response,body){
-    console.log(response.statusCode);
     if(!error && response.statusCode == 200){
-      var result = JSON.parse(body);
-      
-      
       context.succeed(JSON.parse(body));
     }
     else{
